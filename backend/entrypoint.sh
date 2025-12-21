f05cc70e1df83c727dc9dab18b6e47e11c7bd478
clement893 <clement@nukleo.com>
2025-12-21 06:22:48 -0500

    Trigger rebuild: update entrypoint.sh comment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 # Use PORT environment variable if set, otherwise default to 8000
 PORT=${PORT:-8000}
 
-# Start Gunicorn
+# Start Gunicorn with dynamic port support
 exec gunicorn -w 4 -b 0.0.0.0:$PORT --timeout 120 app.main:app
 
