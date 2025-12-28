eb6add64836bcccbac10564c96f2d9f5c61df1ba
clement893 <clement@nukleo.com>
2025-12-28 09:56:25 -0500

    fix(notifications): Update notifications index to export connected components

--- apps/web/src/components/notifications/index.ts
+++ apps/web/src/components/notifications/index.ts
@@ -3,10 +3,14 @@
  */
 
 export { default as NotificationCenter } from './NotificationCenter';
+export { default as NotificationCenterConnected } from './NotificationCenterConnected';
 export { default as NotificationBell } from './NotificationBell';
+export { default as NotificationBellConnected } from './NotificationBellConnected';
 export { default as NotificationSettings } from '../settings/NotificationSettings';
 
 export type { NotificationCenterProps } from './NotificationCenter';
 export type { NotificationBellProps } from './NotificationBell';
+export type { NotificationBellConnectedProps } from './NotificationBellConnected';
+export type { NotificationCenterConnectedProps } from './NotificationCenterConnected';
 export type { NotificationSettingsProps, NotificationSettingsData } from '../settings/NotificationSettings';
 
