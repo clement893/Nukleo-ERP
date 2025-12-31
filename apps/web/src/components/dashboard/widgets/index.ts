7432db41a4dc723e7382cce7626be46c29c11ff2
clement893 <clement@nukleo.com>
2025-12-31 15:48:55 -0500

    fix: Export de tous les widgets dans index.ts

--- apps/web/src/components/dashboard/widgets/index.ts
+++ apps/web/src/components/dashboard/widgets/index.ts
@@ -3,7 +3,21 @@
  */
 
 export { OpportunitiesListWidget } from './OpportunitiesListWidget';
+export { OpportunitiesPipelineWidget } from './OpportunitiesPipelineWidget';
 export { ClientsCountWidget } from './ClientsCountWidget';
+export { ClientsGrowthWidget } from './ClientsGrowthWidget';
+export { TestimonialsCarouselWidget } from './TestimonialsCarouselWidget';
 export { ProjectsActiveWidget } from './ProjectsActiveWidget';
+export { ProjectsStatusWidget } from './ProjectsStatusWidget';
+export { TasksKanbanWidget } from './TasksKanbanWidget';
+export { TasksListWidget } from './TasksListWidget';
 export { RevenueChartWidget } from './RevenueChartWidget';
+export { ExpensesChartWidget } from './ExpensesChartWidget';
+export { CashFlowWidget } from './CashFlowWidget';
 export { KPICustomWidget } from './KPICustomWidget';
+export { GoalsProgressWidget } from './GoalsProgressWidget';
+export { GrowthChartWidget } from './GrowthChartWidget';
+export { EmployeesCountWidget } from './EmployeesCountWidget';
+export { WorkloadChartWidget } from './WorkloadChartWidget';
+export { UserProfileWidget } from './UserProfileWidget';
+export { NotificationsWidget } from './NotificationsWidget';
